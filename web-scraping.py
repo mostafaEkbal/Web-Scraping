967fa8728572261a38972e1246cd98c3349347d5
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2022-12-28 00:11:07 +0200

    parsing the source code for the page

--- web-scraping.py
+++ web-scraping.py
@@ -1,3 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+
+date = input(
+    'Enter a date for the matches you want to see, following this format MM/DD/YY: ')
+page = requests.get(f'https://www.yallakora.com/match-center/?date={date}')
+
+
+def gettingMatchesData(page):
+    soup = BeautifulSoup(page.content, 'lxml')
+    print(soup)
+
+
+gettingMatchesData(page)
